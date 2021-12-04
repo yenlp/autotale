98c495b4474edf8d9da4f48004224ccfa9f56d78
Yen Le <lephuyen@gmail.com>
2021-12-04 10:37:59 +0700

    - revert VM name

--- v2/constant.py
+++ v2/constant.py
@@ -2,7 +2,7 @@ RED = 0
 GREEN = 1
 BLUE = 2
 
-#VM_NAME='AutoBot' #Yen
+VM_NAME='AutoBot' #Yen
 #VM_NAME='AutoTale' #Yen
-VM_NAME='Wartale' #Yen
+#VM_NAME='Wartale' #Yen
 # VM_NAME='192.168.126.128'  #Long
